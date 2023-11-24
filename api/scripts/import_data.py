e4c2e8aaba99b018b832650f7c7cabc234670f3f
SKWCE2023 <shubham.expert@kirtikumarar.com>
2023-11-24 11:50:56 +0530

    Adds script to create temporary administrators

--- api/scripts/import_data.py
+++ api/scripts/import_data.py
@@ -1,6 +1,7 @@
 from .. import views
 
 def run():
-    # views.import_services_data()
+    views.import_services_data()
     views.import_users_data()
-    # views.import_customers_data()
+    views.import_customers_data()
+    views.create_temp_administrators()
